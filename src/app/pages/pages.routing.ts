b5784af3e6a07547b2d9f9c3d4529aa732c58d85
JFernandoHerreraC <fernando_ver908@hotmail.com>
2021-04-09 18:57:01 -0500

    adding files routing

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,32 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { ProgressComponent } from '../pages/progress/progress.component';
+import { Grafica1Component } from '../pages/grafica1/grafica1.component';
+import { PagesComponent } from '../pages/pages.component';
+
+
+const routes: Routes = [
+    {
+        path: 'dashboard/',
+        component: PagesComponent,
+        children: [
+            { path: '', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'grafic_one', component: Grafica1Component }
+        ]
+    },
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule { }
