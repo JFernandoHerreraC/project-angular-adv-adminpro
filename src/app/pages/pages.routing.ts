087b73cbede2b6deb64b4cd9c0b2d3f4d4949b6e
JFernandoHerreraC <fernando_ver908@hotmail.com>
2021-04-09 19:12:47 -0500

    The problem of child routes was fixed

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -9,7 +9,7 @@ import { PagesComponent } from '../pages/pages.component';
 
 const routes: Routes = [
     {
-        path: 'dashboard/',
+        path: 'dashboard',
         component: PagesComponent,
         children: [
             { path: '', component: DashboardComponent },
